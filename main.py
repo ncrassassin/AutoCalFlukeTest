ecc8b237e1a7829c460d638482635f6e2a387bc7
ncrassassin <hemantn76@gmail.com>
2025-03-08 13:27:23 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from views.main_window import TestApp
+
+def main():
+    app = QApplication(sys.argv)
+    window = TestApp()
+    window.show()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
